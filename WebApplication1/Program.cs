f701767569ee6df49e02bf49d010a2c340a1ee92
Manuel Ramirez <manuel777@outlook.com>
2024-04-25 16:29:58 -0600

    swagger fix

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,24 +14,14 @@ builder.Services.AddApiVersioning(options =>
     options.ReportApiVersions = true;
 });
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
-
-    // Add support for XML comments
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    options.IncludeXmlComments(xmlPath);
-});
+builder.Services.AddMvcCore();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API v1");
-    options.RoutePrefix = string.Empty; // Set the Swagger UI at the root URL
-});
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
