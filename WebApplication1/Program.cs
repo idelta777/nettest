9ef4cf2ec4964d003abdab8ee6ad6eb989d9b090
Manuel Ramirez <manuel777@outlook.com>
2024-04-25 16:03:27 -0600

    fix 2

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,7 +2,7 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddAzureKeyVault(new Uri(builder.Configuration["KeyVaultURI"]), new DefaultAzureCredential());
+builder.Configuration.AddAzureKeyVault(new Uri(builder.Configuration["VaultURI"]), new DefaultAzureCredential());
 
 var app = builder.Build();
 
